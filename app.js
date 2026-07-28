b67b9b223da11e35ce58031e291693bd4f60d872
dawnofgod <arunpoudel9957@gmail.com>
2026-07-28 19:05:39 +0545

    remove styleswitcher

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
-styleSwitcherToggle.addEventListener('click', () => { document.querySelector('.style-switcher').classList.toggle('open'); })
+// const styleSwitcherToggle = document.querySelector('.style-switcher-toggler');
+// styleSwitcherToggle.addEventListener('click', () => { document.querySelector('.style-switcher').classList.toggle('open'); })
 
 window.addEventListener('scroll', () => { if(document.querySelector('.style-switcher').classList.contains('open')) { document.querySelector('.style-switcher').classList.remove('open'); } })
 
